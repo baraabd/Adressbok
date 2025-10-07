619be7427c30f14dc247ee440c3673bcd3608b06
lukamil1998 <luka_milanovic@rocketmail.com>
2025-10-07 11:02:36 +0200

    Add DeleteContact feature with WinForms UI

--- AddressBookProject/Form1.Designer.cs
+++ AddressBookProject/Form1.Designer.cs
@@ -2,15 +2,14 @@
 {
     partial class Form1
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        // ListBox to show contacts
+        private System.Windows.Forms.ListBox listBoxContacts;
+
+        // Button to delete selected contact
+        private System.Windows.Forms.Button btnDelete;
+
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,16 +21,45 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+
+            this.listBoxContacts = new System.Windows.Forms.ListBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+
+            this.SuspendLayout();
+
+            // 
+            // listBoxContacts
+            // 
+            this.listBoxContacts.FormattingEnabled = true;
+            this.listBoxContacts.ItemHeight = 15;
+            this.listBoxContacts.Location = new System.Drawing.Point(12, 12);
+            this.listBoxContacts.Name = "listBoxContacts";
+            this.listBoxContacts.Size = new System.Drawing.Size(300, 199);
+            this.listBoxContacts.TabIndex = 0;
+
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(12, 220);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(300, 30);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.Text = "Ta bort kontakt";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(330, 270);
+            this.Controls.Add(this.listBoxContacts);
+            this.Controls.Add(this.btnDelete);
+            this.Name = "Form1";
+            this.Text = "Adressbok";
+            this.ResumeLayout(false);
         }
 
         #endregion
--- AddressBookProject/Form1.cs
+++ AddressBookProject/Form1.cs
@@ -1,10 +1,50 @@
+using System;
+using System.Windows.Forms;
+
 namespace AddressBookProject
 {
     public partial class Form1 : Form
     {
+        private AddressBook addressBook;
+
         public Form1()
         {
             InitializeComponent();
+
+            // Skapar AddressBook och laddar hårdkodade kontakter
+            addressBook = new AddressBook();
+
+            // Uppdatera ListBox med kontakter
+            UpdateContactList();
+        }
+
+        // Uppdatera ListBox med aktuella kontakter
+        private void UpdateContactList()
+        {
+            listBoxContacts.Items.Clear();
+            foreach (var c in addressBook.ContactsList)
+            {
+                listBoxContacts.Items.Add(c.Name);
+            }
+        }
+
+        // Delete-knapp klick
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (listBoxContacts.SelectedItem != null)
+            {
+                string nameToDelete = listBoxContacts.SelectedItem.ToString();
+
+                // Anropar DeleteContact i AddressBook
+                addressBook.DeleteContact(nameToDelete);
+
+                // Uppdatera ListBox efter radering
+                UpdateContactList();
+            }
+            else
+            {
+                MessageBox.Show("Välj en kontakt först.");
+            }
         }
     }
 }
